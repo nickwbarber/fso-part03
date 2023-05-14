f57d51749385d0a5c0b7b971ec84b093476b2d88
nickwbarber <nickwbarber@gmail.com>
2023-05-14 19:26:44 +0800

    GET a person by id

--- index.js
+++ index.js
@@ -16,6 +16,16 @@ app.get('/api/info', (req, res) => {
   `)
 })
 
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(person => person.id === id)
+  if (person) {
+    res.json(person)
+  } else {
+    res.status(404).end()
+  }
+})
+
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
