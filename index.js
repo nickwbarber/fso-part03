7bfa49ade8a4960e1e67c82f1f5c36145d4624c2
nickwbarber <nickwbarber@gmail.com>
2023-05-14 19:39:03 +0800

    Add DELETE /person/:id endpoint

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-const { persons } = require('./data.js')
+let { persons } = require('./data.js')
 
 
 app.get('/api/persons', (req, res) => {
@@ -24,6 +24,12 @@ app.get('/api/persons/:id', (req, res) => {
   } else {
     res.status(404).end()
   }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(person => person.id !== id)
+  res.status(204).end()
 })
 
 
