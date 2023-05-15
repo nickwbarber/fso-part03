cc75671b0e9597a0d8b2d3ba15e410fec393e19c
nickwbarber <nickwbarber@gmail.com>
2023-05-15 17:36:55 +0800

    Show request body in logs

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const express = require('express')
 const app = express()
 
 app.use(express.json())
-app.use(morgan('tiny'))
+
+morgan.token('body', req => `body: ${JSON.stringify(req.body)}`)
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let { persons } = require('./data.js')
 
