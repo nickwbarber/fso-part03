386355dba2110332c3c59214dfd9e405c21fb29d
nickwbarber <nickwbarber@gmail.com>
2023-05-24 18:40:54 +0800

    Allow for two user modes
    1. get db info
    2. add a record
    
    closes #2

--- mongo.js
+++ mongo.js
@@ -1,20 +1,5 @@
 const mongoose = require('mongoose')
 
-// get password from command line input
-if (process.argv.length<5) {
-  console.log('usage: node mongo.js <password> <name> <number>')
-  process.exit(1)
-}
-
-const [ password, name, phoneNumber ] = process.argv.slice(2)
-
-// connect to db
-const appName = 'phonebook-app'
-const url = 
-  `mongodb+srv://nick:${password}@cluster0.rbzkdxs.mongodb.net/${appName}?retryWrites=true&w=majority`
-mongoose.set('strictQuery', false)
-mongoose.connect(url)
-
 // define phonebook records
 const recordSchema = new mongoose.Schema({
   name: String,
@@ -22,12 +7,60 @@ const recordSchema = new mongoose.Schema({
 })
 const Record = mongoose.model('Record', recordSchema)
 
-// add record from command line input
-const record = new Record({
-  name: name,
-  number: phoneNumber,
-})
-record.save().then(result => {
-  console.log(`added ${name}'s number ${phoneNumber} to phonebook`)
-  mongoose.connection.close()
-})
+// connect to db
+const connectToDB = (password) =>{
+  const appName = 'phonebook-app'
+  const url =
+    `mongodb+srv://nick:${password}@cluster0.rbzkdxs.mongodb.net/${appName}?retryWrites=true&w=majority`
+  mongoose.set('strictQuery', false)
+  mongoose.connect(url)
+}
+
+const GET_INFO = 3
+const ADD_RECORD = 5
+const MODE = process.argv.length
+
+// do stuff
+switch (MODE) {
+  case GET_INFO: {
+    const password = process.argv[2]
+
+    connectToDB(password)
+    
+    // get all records
+    Record.find({}).then(result => {
+      console.log('phonebook:')
+      result.map(record => {
+        console.log(`${record.name} ${record.number}`)
+      })
+      mongoose.connection.close()
+    })
+    break
+  }
+
+  case ADD_RECORD: {
+    const [ password, name, phoneNumber ] = process.argv.slice(2)
+
+    connectToDB(password)
+    
+    // add record from command line input
+    const record = new Record({
+      name: name,
+      number: phoneNumber,
+    })
+
+    // log success and close connection
+    record.save().then(result => {
+      console.log(`added ${name}'s number ${phoneNumber} to phonebook`)
+      mongoose.connection.close()
+    })
+    break
+  }
+
+  default: {
+    console.log('usage: node mongo.js <password> <name> <number>')
+    process.exit(1)
+    break
+  }
+
+}
