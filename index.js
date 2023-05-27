dac21fb4ac7e7c98672f9c0068431fb7a1378e00
nickwbarber <nickwbarber@gmail.com>
2023-05-27 10:36:16 +0800

    Fix code formatting

--- index.js
+++ index.js
@@ -131,13 +131,13 @@ app.use((req, res) => {
 // error handler
 app.use((err, req, res, next) => {
   console.error(err.message)
-  if (err.name === "CastError") {
+  if (err.name === 'CastError') {
     return res.status(400).send({ error: 'malformatted id'})
   }
-  if (err.name === "ValidationError") {
+  if (err.name === 'ValidationError') {
     return res.status(400).send({ error: err.message })
   }
-  next(error)
+  next(err)
 })
 
 const PORT = process.env.PORT || 3001
